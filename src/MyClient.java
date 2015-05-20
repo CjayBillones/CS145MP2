4c4f76d80fe8c5f50dc262e6be12e3d994d76808
Cjay <billonesciprian08@gmail.com>
2015-05-21 00:00:44 +0800

    Add ip and port validation

--- src/MyClient.java
+++ src/MyClient.java
@@ -8,8 +8,16 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MyClient extends JFrame{
 
+	private static final String IPADDRESS_PATTERN = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+																									"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+																									"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+																									"([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+	
 	public static int WIDTH = 800;
 	public static int HEIGHT = 600;
 
@@ -19,12 +27,12 @@ public class MyClient extends JFrame{
 	Thread receiver;
 	boolean flag;
 
-	public MyClient(){
+	public MyClient(String ip, int port){
 
 		/** Connection Initialization **/
 		try{
 			this.flag = false;
-			this.s = new Socket("127.0.0.1", 8888);
+			this.s = new Socket(ip, port);
 			this.conn = new MyConnection(s);
 			this.receiver = new Thread(new ClientReceiver());
 			this.receiver.start();
@@ -35,8 +43,28 @@ public class MyClient extends JFrame{
 	}
 
 	public static void main(String args[]){
-		MyClient c = new MyClient();
-		MarioWindow w = new MarioWindow(c);
+		try{
+			String ip = JOptionPane.showInputDialog("Enter IP Address: ", "127.0.0.1");
+			int port = Integer.parseInt(JOptionPane.showInputDialog("Enter Port Number: ", "8888"));
+			
+			//if(validateIP(ip)){
+				//MyClient c = new MyClient(ip, port);
+				MyClient c = new MyClient("127.0.0.1", 8888);
+				MarioWindow w = new MarioWindow(c);
+			//}
+			//else{
+				//System.out.println("Client: Invalid IP Address");
+				//System.exit(1);
+			//}
+		}catch(NumberFormatException e){
+			System.out.println("Client: Invalid Port Number");
+			e.printStackTrace();
+		}
+	}
+
+	public static boolean validateIP(String ip){
+		Pattern pattern = Pattern.compile(IPADDRESS_PATTERN);
+		return pattern.matcher(ip).matches();
 	}
 
 	private class ClientReceiver implements Runnable{
